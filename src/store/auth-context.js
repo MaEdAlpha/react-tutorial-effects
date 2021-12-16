60ac53ece0df0d45abd632e3c96cdfd4dfa336d5
Ryan <72553021+MaEdAlpha@users.noreply.github.com>
2021-12-16 10:16:58 +0700

    Refer to AuthContext.js file
    One way of using Context.Consumers

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //use context object to avoid prop chaining. 
-// need to PROVIDE/CONSUME(hook-in/listen to)
+// need to PROVIDE/CONSUME(hook-in/listen to).
 //need to wrap components with context to allow the components to access the context. checkout App.js..
 const AuthContext = React.createContext({
     isLoggedIn:false,
